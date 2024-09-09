6256bbb1e975909d4925c74b58277d13b02826b6
Habibul Bashar <tj7279897@gmail.com>
2024-09-09 20:23:50 +0800

    Time: 1 ms (99.51%), Space: 45.4 MB (12.79%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        
+        List<String> output = new ArrayList<>();
+
+        for (int i = 1; i < n + 1; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                output.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                output.add("Fizz");
+            } else if (i % 5 == 0) {
+                output.add("Buzz");
+            } else {
+                output.add(String.valueOf(i));
+            }
+        }
+        return output;
+    }
+}
