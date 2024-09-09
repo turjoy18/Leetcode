6ab33248ca6220ee003d8008ffba2671ff15acdc
Habibul Bashar <tj7279897@gmail.com>
2024-09-09 19:28:59 +0800

    Time: 0 ms (100.00%), Space: 40.2 MB (85.60%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numberOfSteps(int num) {
+        
+        int step = 0;
+        
+        while (num != 0) {
+            if (num % 2 == 0) {
+                num /= 2;
+                step += 1;
+            } else {
+                num -= 1;
+                step += 1;
+            }
+        }
+        return step;
+    }
+}
