506060f129f7392d975a50f5f0dace0ce44dce1c
Habibul Bashar <tj7279897@gmail.com>
2024-09-20 17:38:50 +0800

    Time: 0 ms (100.00%), Space: 40.8 MB (6.21%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int climbStairs(int n) {
+        
+        int a =  0;
+        int b = 1;
+        
+        int ways = 0;
+        for (int i = 0; i < n; i++) {
+            ways = a + b;
+            a = b;
+            b = ways;
+        }
+        return ways;
+        
+    }
+}
