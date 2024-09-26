abc0b486a9e3547dc579d6a588eb3e8293a461ee
Habibul Bashar <tj7279897@gmail.com>
2024-09-27 01:26:07 +0800

    Time: 7 ms (100.00%), Space: 43.8 MB (84.59%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        
+        if (n != 0) {
+            while (n % 3 == 0) {
+                n /= 3;
+            }
+
+            return n  == 1;
+        } else {
+            return false;
+        }
+    }
+}
