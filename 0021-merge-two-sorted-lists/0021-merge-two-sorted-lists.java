5c3bc183075d77e9a086b63da5542814642ac126
Habibul Bashar <tj7279897@gmail.com>
2024-11-22 02:48:50 +0800

    Update 0021-merge-two-sorted-lists.java

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -25,15 +25,11 @@ class Solution {
             curr = curr.next;
         }
         
-        if (list1 != null) {
+        if (list1 != null)
             curr.next = list1;
-            list1 = list1.next;
-        }
         
-        if (list2 != null) {
+        if (list2 != null)
             curr.next = list2;
-            list2 = list2.next;
-        }
         
         return dummyHead.next;
         
